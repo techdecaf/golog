737582b5d4b8adc6e05756ca89757d4491477964
jon.ward <jon@techdecaf.com>
2019-09-03 09:27:48 -0700

    expose print, improve tests

--- golog.go
+++ golog.go
@@ -3,7 +3,6 @@ package golog
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/fatih/color"
 )
@@ -15,31 +14,32 @@ type Log struct {
 
 // LogMessage message structure
 type LogMessage struct {
-	Action  string
-	Message interface{}
+	Action   string
+	Message  interface{}
+	ExitCode int
 }
 
 // Info displays information about the program durring runtime.
 func (log *Log) Info(action string, text interface{}) string {
-	return log.print(LogMessage{Action: action, Message: text}, false)
+	return log.Print(LogMessage{Action: action, Message: text})
 }
 
 // Fatal logs to os.Stderr and exits 1
 func (log *Log) Fatal(action string, err interface{}) string {
-	return log.print(LogMessage{Action: action, Message: err}, true)
+	return log.Print(LogMessage{Action: action, Message: err, ExitCode: 1})
 }
 
-func (log *Log) print(m LogMessage, exit bool) string {
+// Print is a helper function for log.Info and log.Fatal,
+// but can be called directly if you want more control over the exit code.
+func (log *Log) Print(m LogMessage) string {
 	msg := fmt.Sprintf("[%s][%s] %v", log.Name, m.Action, m.Message)
 
 	printInfo := color.New(color.FgGreen)
 	printError := color.New(color.FgRed)
 
-	m.Action = strings.ToLower(m.Action)
-
-	if exit {
+	if m.ExitCode > 0 {
 		printError.Fprintln(os.Stderr, msg)
-		os.Exit(1)
+		os.Exit(m.ExitCode)
 	}
 	printInfo.Fprintln(os.Stdout, msg)
 
--- golog_test.go
+++ golog_test.go
@@ -6,10 +6,21 @@ var log = Log{
 	Name: "testing",
 }
 
-func TestHello(t *testing.T) {
+func TestLogInfo(t *testing.T) {
 	want := "[testing][action] message"
 
 	if got := log.Info("action", "message"); got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
+		t.Errorf("log.Info() = %q, want %q", got, want)
+	}
+}
+
+func TestLogPrint(t *testing.T) {
+	want := "[testing][log.Print] exit"
+	msg := LogMessage{
+		Action:  "log.Print",
+		Message: "exit",
+	}
+	if got := log.Print(msg); got != want {
+		t.Errorf("log.Print() = %q, want %q", got, want)
 	}
 }
